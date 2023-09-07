f2df16bb91061b33449ddd4ac236e5c3a733f92c
ToddW317 <wooten00@gmail.com>
2023-09-07 00:49:03 -0700

    gallery update

--- app.js
+++ app.js
@@ -1,51 +1,3 @@
-// Setting up the Variables
-var bars = document.getElementById("nav-action");
-var nav = document.getElementById("nav");
-
-
-//setting up the listener
-bars.addEventListener("click", barClicked, false);
-
-
-//setting up the clicked Effect
-function barClicked() {
-  bars.classList.toggle('active');
-  nav.classList.toggle('visible');
-}
-
-$('.toggle-menu').click (function(){
-	$(this).toggleClass('active');
-	$('#menu').toggleClass('open');
-  });
-
-
-  const nav = document.querySelector(".nav"),
-  searchIcon = document.querySelector("#searchIcon"),
-  navOpenBtn = document.querySelector(".navOpenBtn"),
-  navCloseBtn = document.querySelector(".navCloseBtn");
-
-searchIcon.addEventListener("click", () => {
-  nav.classList.toggle("openSearch");
-  nav.classList.remove("openNav");
-  if (nav.classList.contains("openSearch")) {
-    return searchIcon.classList.replace("uil-search", "uil-times");
-  }
-  searchIcon.classList.replace("uil-times", "uil-search");
-});
-
-navOpenBtn.addEventListener("click", () => {
-  nav.classList.add("openNav");
-  nav.classList.remove("openSearch");
-  searchIcon.classList.replace("uil-times", "uil-search");
-});
-navCloseBtn.addEventListener("click", () => {
-  nav.classList.remove("openNav");
-});
-
-
-
-//Gallery
-
 class CS_GalleryFilter {
 	filtersSelector = '.cs-button';
 	galleriesSelector = '.cs-gallery';
@@ -87,5 +39,3 @@ class CS_GalleryFilter {
 
 new CS_GalleryFilter();
                                 
-                                
-
